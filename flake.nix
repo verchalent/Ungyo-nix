ca454064cc784e00ddede43e747eb004ce40aa0b
Pestilent <pestilent@amaterasu>
2024-07-08 21:01:53 -0400

    moving to unstable for home manager packages

--- flake.nix
+++ flake.nix
@@ -1,14 +1,12 @@
 {
   description = "My WIP MacOS flake";
   inputs = {
-    # Where we get most of our software. Giant mono repo with recipes
-    # called derivations that say how to build software.
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
     # Manages configs links things into your home directory
     home-manager.url = "github:nix-community/home-manager/master";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs-unstable";
 
     # Controls system level software and settings including fonts
     darwin.url = "github:lnl7/nix-darwin";
