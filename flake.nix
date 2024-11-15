bf1f7f29a0fb6c706397859b096af6fb584f7ab6
Justin <someemail@blahblah.com>
2024-11-15 13:30:45 -0500

    Moving to unstable for now

--- flake.nix
+++ flake.nix
@@ -3,12 +3,12 @@
   inputs = {
   # Moving to staging-next 11.15.24 due to https://github.com/NixOS/nixpkgs/pull/350374
   #  nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    nixpkgs.url = "github:nixos/nixpkgs/staging-next";
+  #  nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/unstable";
 
     # Manages configs links things into your home directory
-    home-manager.url = "github:nix-community/home-manager/release-24.05"; #Aligning back to current
-    # home-manager.url = "github:nix-community/home-manager/master";
+  #  home-manager.url = "github:nix-community/home-manager/release-24.05"; #Aligning back to current
+    home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     # Controls system level software and settings including fonts
