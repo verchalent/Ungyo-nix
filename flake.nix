7cc6736bd8bbeac708867c178eb0eae49eff9d0d
Justin <someemail@blahblah.com>
2024-11-15 13:22:49 -0500

    Moving to staging due to bug

--- flake.nix
+++ flake.nix
@@ -1,11 +1,14 @@
 {
   description = "My WIP MacOS flake";
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  # Moving to staging-next 11.15.24 due to https://github.com/NixOS/nixpkgs/pull/350374
+  #  nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+  #  nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/staging-next";
 
     # Manages configs links things into your home directory
-    home-manager.url = "github:nix-community/home-manager/release-24.05";
+    # home-manager.url = "github:nix-community/home-manager/release-24.11"; #Aligning back to current
+    home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     # Controls system level software and settings including fonts
