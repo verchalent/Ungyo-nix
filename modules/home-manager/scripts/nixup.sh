d2ef56c5ece889e2d3fea1d33c08e006711dad7b
Justin <someemail@blahblah.com>
2025-06-09 09:10:37 -0400

    Comment out wallpaper setting command in nixup script

--- modules/home-manager/scripts/nixup.sh
+++ modules/home-manager/scripts/nixup.sh
@@ -2,8 +2,7 @@
 # Script to update Brew, NixOS and ensure defaults applied
 
 # Set Wallpaper - no fun Nix way to do this yet :)
-osascript -e "tell application \"Finder\" to set desktop picture to POSIX file \"$(dirname "$PWD")/wallpaper/JackFrost.jpg\""
-
+# osascript -e "tell application \"Finder\" to set desktop picture to POSIX file \"$(dirname "$PWD")/wallpaper/JackFrost.jpg\"" #Trowing an error, so commenting out for now
 # Update and clean up Brew
 brew update 
 brew upgrade 
